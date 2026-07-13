acb60e1eebc088982cde0d746b20c27f840ffe6a
erikjoachim <erikneuhofer@gmail.com>
2026-07-13 02:26:15 +0200

    refactor(ui): reorganize components into categorized directories
    - core/: TBadge, TButton
    - typography/: THeaderText
    - layout/: TCard, TStatsCard
    - Remove TSectionHeader (superseded by THeaderText)

--- ui/src/vue/index.ts
+++ ui/src/vue/index.ts
@@ -1,7 +1,6 @@
-export { default as TBadge } from "./badge/TBadge.vue";
-export { default as TButton } from "./button/TButton.vue";
-export { default as TSectionHeader } from "./section-header/TSectionHeader.vue";
-export { default as TCard } from "./card/TCard.vue";
+export { default as TBadge } from "./core/badge/TBadge.vue";
+export { default as TButton } from "./core/button/TButton.vue";
+export { default as TCard } from "./layout/card/TCard.vue";
 export { default as THeaderText } from "./typography/header-text/THeaderText.vue";
 export { default as TStatsCard } from "./layout/stats-card/TStatsCard.vue";
 export type { HighlightSegment } from "../shared/parser/highlight";
--- ui/src/vue/layout/stats-card/TStatsCard.vue
+++ ui/src/vue/layout/stats-card/TStatsCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import TCard from "../../card/TCard.vue";
+import TCard from "../card/TCard.vue";
 
 withDefaults(
   defineProps<{
