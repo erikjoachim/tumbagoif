02f40c610788c904085113bea819c0e9a6490958
Erik Neuhofer <erikneuhofer@gmail.com>
2026-08-29 12:31:03 +0200

    feat(ui): social links with icons

--- ui/src/vue/index.ts
+++ ui/src/vue/index.ts
@@ -1,6 +1,8 @@
 export { default as TBadge } from "./core/badge/TBadge.vue";
 export { default as TButton } from "./core/button/TButton.vue";
 export { default as TCard } from "./layout/card/TCard.vue";
+export { default as TSocialLinks } from "./layout/social-links/TSocialLinks.vue";
+export type { SocialLink } from "./layout/social-links/TSocialLinks.vue";
 export { default as THeaderText } from "./typography/header-text/THeaderText.vue";
 export { default as TText } from "./typography/text/TText.vue";
 export { default as TStatsCard } from "./layout/stats-card/TStatsCard.vue";
