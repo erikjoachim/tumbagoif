59df5c44990766f6d359d17752c0257ac6a5eead
Erik Neuhofer <erikneuhofer@gmail.com>
2026-08-30 21:13:47 +0200

    chore: run format command
    `npm run format` from root directory

--- auth/server/src/index.ts
+++ auth/server/src/index.ts
@@ -16,6 +16,8 @@ serve(
     port,
   },
   (info) => {
-    console.log(`Tumba GOIF Identity auth server running at ${env.BETTER_AUTH_URL} (port ${info.port})`);
+    console.log(
+      `Tumba GOIF Identity auth server running at ${env.BETTER_AUTH_URL} (port ${info.port})`,
+    );
   },
 );
